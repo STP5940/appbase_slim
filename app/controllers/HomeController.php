9c44b93280f2ab81bbd12e6eb10f442cabca0891
stp5940 <sitthipong114@gmail.com>
2019-07-05 12:01:49 +0700

    update demo select users

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\controllers;
 
-use App\Models\Sample;
 use App\Models\Users;
+use App\controllers\Controller;
+use Illuminate\Database\Capsule\Manager as DB;
 
 class HomeController extends Controller {
     /**
@@ -11,11 +12,18 @@ class HomeController extends Controller {
 
     public function index($request, $response, $args)
     {
+        $id   = $args['id'];
 
-        $use   =  Users::find($args['id']);
+        $use   =  Users::find($id);
         $email =  $this->json($use->email);
 
-        $id   = $args['id'];
+        $test = DB::table('Users')->where('id',$id)->get();
+        echo "<pre>";
+        print_r($test);
+        echo "</pre>";
+        echo $test[0]->password;
+        echo "<br><br>";
+
         $sth  = $this->db->prepare("SELECT * FROM USERS WHERE id=? ");
         $sth->bindParam(1, $id);
         $sth->execute();
--- src/dependencies.php
+++ src/dependencies.php
@@ -23,11 +23,11 @@ return function (App $app) {
     // Connection mysql DB PDO
     // connect mysql only one
     $container['mysql'] = function ($c) {
-      $settings = $c->get('settings')['db'];
-      $pdo = new PDO("mysql:host=".$settings["host"].";dbname=".$settings['database'],$settings['username'],$settings['password']);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-      return $pdo;
+        $settings = $c->get('settings')['db'];
+        $pdo = new PDO("mysql:host=".$settings["host"].";dbname=".$settings['database'],$settings['username'],$settings['password']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
     };
 
     // Using Eloquent ORM
@@ -37,7 +37,7 @@ return function (App $app) {
     $capsule->bootEloquent();
     //pass the connection to global container (created in previous article)
     $container['db'] = function ($container) use ($capsule){
-       return $capsule;
+        return $capsule;
     };
 
 };
